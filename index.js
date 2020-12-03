0872feff9a9fdb4c21db16bdf04c8b4ac95f577f
Victoria Nayshul <vbnaish@yandex.ru>
2020-12-03 18:40:28 +0300

    add function lodash

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello, Hexlet!")
+console.log("Hello, Hexlet!");
+
+import _ from "lodash";
+const a =_.capitalize("hello");
+console.log(a);
+const b =_.intersection([2, 1], [2, 3]);
+console.log(b);
+console.log(_.last(['one', 'two']));
