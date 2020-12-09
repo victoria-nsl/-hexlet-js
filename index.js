6389342fed611be2f78e85df92d148ffd336a642
Victoria Nayshul <vbnaish@yandex.ru>
2020-12-09 19:08:58 +0300

    learn eslint too

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-console.log("Hello, Hexlet!");
+import _ from 'lodash';
 
-import _ from "lodash";
-const a = _.capitalize("hello");
+console.log('Hello, Hexlet!');
+const a = _.capitalize('hello');
 console.log(a);
 const b = _.intersection([2, 1], [2, 3]);
 console.log(b);
-console.log(_.last(["one", "two"]));
+console.log(_.last(['one', 'two']));
